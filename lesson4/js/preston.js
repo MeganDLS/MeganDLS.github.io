2d53bc8047d777ce25bf266a07f485bbf30447b1
megandls <megandeleon5@gmail.com>
2021-05-12 00:23:42 -0600

    adding all the pages

---
+++ lesson4/js/preston.js
@@ -0,0 +1,6 @@
+let d = new Date();
+document.querySelector('#copyrightyear').textContent = d.getFullYear();
+document.querySelector('#modified').textContent = document.lastModified;
+
+// const d = new Date();
+// document.querySelector('#currentyear').textContent = d.toDateString();
