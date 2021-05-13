2f34ce2af81f1a9919b1f5da1f41c2fd1c9cfd9e
megandls <megandeleon5@gmail.com>
2021-05-13 11:21:23 -0600

    optimizing image

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -1,6 +1,25 @@
-let d = new Date();
-document.querySelector('#copyrightyear').textContent = d.getFullYear();
-document.querySelector('#modified').textContent = document.lastModified;
+//select the element to manipulate
+const d = docuemnt.querySelector("date");
+
+//current date with that object
+const now = new Date()
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+//document.querySelector('#copyrightyear').textContent = d.getFullYear();
+//document.querySelector('#modified').textContent = document.lastModified;
+
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+
+
+//const d = new Date(2021, 4, 12);
+//document.querySelector('#copyrightyear').textContent = d.getFullYear();
+//document.querySelector('#modified').textContent = document.lastModified;
 
 // const d = new Date();
 // document.querySelector('#currentyear').textContent = d.toDateString();
