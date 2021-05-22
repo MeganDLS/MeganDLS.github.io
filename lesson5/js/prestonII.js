2012a5afb764545bbdba06cd0897e89434050e82
megandls <megandeleon5@gmail.com>
2021-05-22 15:06:57 -0600

    fixing contact block

--- lesson5/js/prestonII.js
+++ lesson5/js/prestonII.js
@@ -14,7 +14,14 @@ const fulldate = new Intl.DateTimeFormat("en-US", {
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 // Hamburger
+// Responsive Hamburger button for the menu items
+const hamburgerButton = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
 
+// add the callback function () is a shortcut, arrow function is => in modern javascript (see js_arrow_function in w3 schools), the false is a kickback to the event listener.
+hamburgerButton.addEventListener('click', () =>
+{navigation.classList.toggle('hide')}, false
+);
 
 //If the menu is shown and then the window is resized, avoid problems by
 // the 760 or the 32.5em
