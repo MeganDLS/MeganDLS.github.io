a0d7dc667e4c87932cbe7fc824eefc0c575264da
megandls <megandeleon5@gmail.com>
2021-05-27 13:56:04 -0600

    adding windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,27 +1,23 @@
 
 //if this is true and this is true call the function. Otherwise just print na
 //DOM listener? use query selector instead of get element by id
-let temp = document.querySelector("nth child of .").innerHTML = wc; // instead of getElementById('highttemp');
-let windspeed = 25;
-let feelTemp = document.getElementById('feelTemp');
-feelTemp.innerHTML = getwindchill(temp, windspeed);
+let temp = parseFloat('#hightemp')
+//document.querySelector('hightemp');
+console.log(temp);
+//innerHTML = wc; // instead of getElementById('highttemp');
+let windspeed = document.querySelector('windspeed');
 
-// Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)."
-// do the parameters need to be outside the function?
-//temp at <= 50 and windspeed > 3 mph
-function getwindchill(temp, speed) {
 
-let airav = 
-
-// wind chill is in ferinheit. Windspeed is to the power of 0.16.
-let windchill = 35.74 + .6215 * airav - 35.75 * Math.pow(windspeed, .16) + .4275 * airav * Math.pow(windspeed, .16);
-console.log(windchill);
-return Math.pow blah blah
-
-windchill= (windchill > airav) ? temp : windchill;
-
-console.log(windchill);
-return windchill;
+if ((hightemp <= 50) && (windspeed > 3)) {
+    document.querySelector('#windchill').innerHTML = getwindchill(temp, windspeed);
 
 }
-// else "N/A"
+    else {
+        document.querySelector('#windchill').innerHTML = "N/A";
+    }
+
+// Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)."
+function getwindchill(hightemp, windspeed) {
+    let wc = 35.74 + .6215 * hightemp - 35.75 * Math.pow(windspeed, .16) + .4275 * hightemp * Math.pow(windspeed, .16);
+    return wc;
+    }
