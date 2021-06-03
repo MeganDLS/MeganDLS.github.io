b0e95d861df8c161b2164ed7bdd610dd9d2cfece
megandls <megandeleon5@gmail.com>
2021-06-03 17:09:46 -0600

    lazy loading done

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,39 +1,42 @@
-//****Lazy Loading****//Create a list
-const imagesToLoad = document.querySelectorAll('img[data-src]');
+//****Lazy Loading****/
 
-//separate out or loop through each image ?
+//Create a list
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+//Separate out or loop through each image
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
 };
-//console.display(image);
+//Loop through each image
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
 
-//do something with each image in the images list
+//Do something with each image in the images list
 const imgOptions = {
     rootmargin: '0px 0px 300px 0px',
     threshold: 0
 };
 
 if ('IntersectionObserver' in Window) {
-    const imgObserver = new IntersectionObserver(items => {
-        items.forEach((item) =>
-        {if (item.isIntersecting) {
-            loadImages(item.target);
-            imgObserver.unobserve(item.target);
-        }
-    });
-        }, imgOptions);
-
-        //load image if necessary
-    imagesToLoad.forEach((img) => {
-            imgObserver.observe(img);
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
         });
-    }
-else {
-        //load all images if not supported
-        const imgObserver = new IntersectionObserver(items => {
-            items.forEach((item) =>
-        imgObserver.unobserve(img));
-    })}
+    });
+    //Load image if necessary
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    //Load all images if not supported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
