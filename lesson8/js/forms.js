054d98f2bfed5b24a1a782e61a63eab7bf083aed
megandls <megandeleon5@gmail.com>
2021-06-12 18:34:16 -0600

    slider

--- lesson8/js/forms.js
+++ lesson8/js/forms.js
@@ -1,2 +1,15 @@
 // full name
 let re = /^[a-z ,.'-]+$/i {1,6};
+
+// style
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
