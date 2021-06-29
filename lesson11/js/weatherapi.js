b72f17f34f05c15b79f65ad976fe40b00c732430
megandls <megandeleon5@gmail.com>
2021-06-28 23:03:23 -0600

    form

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -1,9 +1,9 @@
 // Current Weather Conditions
 // Preston ID: 5604473
-const requestcurrentURL = 
+const requestURL = 
 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=edd40baf43f2b3f80219cb8af5bd05d9';
 
-fetch(requestcurrentURL)
+fetch(requestURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
@@ -18,39 +18,11 @@ fetch(requestcurrentURL)
     document.getElementById('speed').textContent = s;
 
     let wc = (35.74 + (.6215 * t)) - (35.75 * Math.pow(s, .16)) + (.4275 * t * Math.pow(s, .16));
-    let windchill = Math.round(wc);
+    let wchill = Math.round(wc);
 
-    if (wcc >= 50 && s > 3) {
-      document.querySelector('#windchill').innerHTML = windchill;
+    if (wchill >= 50 && s > 3) {
+      document.querySelector('#windchill').innerHTML = wchill;
     } else {
       document.querySelector('#windchill').innerHTML = "N/A";
     }
 });
-
-// Five Day Forecast from 16 days API
-const requestAPI =
-'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=edd40baf43f2b3f80219cb8af5bd05d9';
-
-fetch(requestAPI)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-
-    let day = 1;
-    const dayofweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    const fivedays = jsObject.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
-    console.log(fivedays);
-   
-    fivedays.forEach(x => {
-      let d = new Date(x.dt_txt);
-
-    //   Retrieving Day of the week
-      document.getElementById('day' + day).textContent = dayofweek[d.getDay()];
-    //   Retrieving Current Temperature
-      document.getElementById('data' + day).textContent = x.main.temp;
-    //   Image for conditions
-      document.getElementById('day' + day + 'image').setAttribute('src', `https://openweathermap.org/img/w/${x.weather[0].icon}.png`);
-      day++;
-    })
-  });
