d7ec316cb9461f4a5eb1b68a4c1436078707ba9d
megandls <megandeleon5@gmail.com>
2021-06-29 18:00:22 -0600

    last visited

--- lesson11/js/lastvisited.js
+++ lesson11/js/lastvisited.js
@@ -1,112 +1,29 @@
-var sec = 0;
-var stoptime = true;
+//#daysince
 
+let last; 
 
-function setStart() {
-    if (stoptime == true) {
-        stoptime = false;
-        setStop();
-     }
-}
-
-function stopTimer() {
-    if (stoptime == false) {
-         stoptime = true;
-     }
- }
-
-function setStop() {
-     sec = sec + 1;
+if (localStorage.getItem('date') !== null) {
+    last = new Date(localStorage.getItem('date')); }
+    else {
+        last = new Date();
+        localStorage.setItem('date', String(last));
+    }
+    document.getElementById('daysince').innerHTML = String(last);
     
-}
+let now = new Date();
+console.log(now - last);
 
-function getNumberOfDays() {
-    let day = 60 * 60 * 24;
-    document.querySelector('#daysince').innerHTML = sec/day;
-    setInterval("setStop()", 1000);  
-}
+// One day in milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+const milliseconds = now - last;
 
+// Calculating the time difference between two dates
+//const diffInTime = date2.getTime() - date1.getTime();
 
+// Calculating the no. of days between two dates
+//const diffInDays = Math.round(diffInTime / oneDay);
 
-// -----
+document.getElementById('daysince').innerHTML = Math.trunc(milliseconds / oneDay);
 
-//  function saveCurrentDate(){
-//     const date = new Date();
-//     localStorage.setItem('date', date.toISOString());
-//  }
-// //This shuold be called when the page loads
-//  function getLastVisited() {
-//     let last;
-//      if (localStorage.get('date') !== null )
-//       last = new Date(localStorage.get('date'));
-//      else last = null;
+localStorage.setItem('date', String(now));
 
-//      if (last) {
-//          alert("compute the days");
-//      }
-//      else {
-//          alert("This is the first time visiting the page");
-//      }
-
-//      saveCurrentDate();
-//  }
-//  getLastVisited();
-
-
-
-
-
-//  -------
-//if null save Date.
-//if not null, 
-
-
-// let start = Date.now();
-// console.log(start);
-
-// function getNumberOfDays(start {
-//     let endDay = new Date();
-// let diffrence = endDay - start;
-
-// }
-
-// console.log(b-a);
-// localStorage.s = c;
-// localStorage.e = d;
-
-// let f = Date.parse(localStorage.b);
-
-// From seconds to days
-
-
-// function endDay(){
-//     let endDay = performance.now();
-//     console.log(endDay);
-//     //var t = d.toLocaleTimeString();
-//     return endDay;
-
-// }
-
-//var e = new Date();
-
-// const date1 = createInstance();
-//let start = console.time();
-
-// const date2 = new Date();
-
-//let endDay = Date.now();
-
-// // One day in milliseconds
-// const oneDay = 1000 * 60;  //60 * 24;
-
-// // Calculating the time difference between two dates
-// const diffInTime = date2.getTime() - date1.getTime();
-
-// // Calculating the no. of days between two dates
-// const diffInDays = Math.round(diffInTime / oneDay);
-
-// return diffInDays; 
-
-
-//}
-//let time = getNumberOfDays();
