07fc959e6dde97e8dea0f9b5ddc630ec2ded0906
megandls <megandeleon5@gmail.com>
2021-07-01 23:13:24 -0600

    mavigation line

---
+++ lesson11/js/townss.js
@@ -0,0 +1,25 @@
+const requestURLp = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURLp)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+
+        const towns = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Soda Springs") {
+                let events = document.createElement('div');
+                let h3 = document.createElement('h3');
+                
+                h3.textContent = towns[i].events;
+
+                events.appendChild(h3);
+                
+                document.querySelector('div.upcomings').appendChild(events);
+            }
+        }
+    });
