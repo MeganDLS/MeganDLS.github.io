1924bc60c88bda277ac5f1a274edaf5eecd51e9d
megandls <megandeleon5@gmail.com>
2021-07-02 23:02:26 -0600

    last visited

--- lesson11/js/lastvisited.js
+++ lesson11/js/lastvisited.js
@@ -1,29 +1,18 @@
-//#daysince
+//#daysince in gallery.html
 
-let last; 
+let last;
 
 if (localStorage.getItem('date') !== null) {
-    last = new Date(localStorage.getItem('date')); }
-    else {
-        last = new Date();
-        localStorage.setItem('date', String(last));
-    }
-    document.getElementById('daysince').innerHTML = String(last);
-    
-let now = new Date();
-console.log(now - last);
+    last = new Date(localStorage.getItem('date'));
+} else {
+    last = new Date();
+    localStorage.setItem('date', String(last));
+}
+document.getElementById('daysince').innerHTML = String(last);
 
-// One day in milliseconds
+let now = new Date();
 const oneDay = 1000 * 60 * 60 * 24;
 const milliseconds = now - last;
 
-// Calculating the time difference between two dates
-//const diffInTime = date2.getTime() - date1.getTime();
-
-// Calculating the no. of days between two dates
-//const diffInDays = Math.round(diffInTime / oneDay);
-
 document.getElementById('daysince').innerHTML = Math.trunc(milliseconds / oneDay);
-
 localStorage.setItem('date', String(now));
-
